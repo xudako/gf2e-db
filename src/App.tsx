bea13db8c7c0a063af7d2f72e37ba9cc3148c3e4
xuda <xudacn0@gmail.com>
2025-03-21 13:53:49 -0400

    tailwind migration complete

--- src/App.tsx
+++ src/App.tsx
@@ -10,58 +10,9 @@ import Home from "./pages/Home";
 import CharacterGrid from "./pages/CharacterGrid";
 import CharacterOverlay from "./pages/CharacterOverlay";
 import WeaponGrid from "./pages/WeaponGrid";
-import { GlobalStyles } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { Chr } from "./types";
 import { characters } from "./data/data";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#373e44",
-      contrastText: "#ffffff",
-    },
-    secondary: {
-      main: "#f26c1c",
-      contrastText: "#ffffff",
-    },
-    background: {
-      default: "#999999",
-      paper: "#dddddd",
-    },
-    text: {
-      primary: "#373e44",
-      secondary: "#666666",
-    },
-    raritySSR: {
-      main: "#de9e01",
-    },
-    raritySR: {
-      main: "#7967ba",
-    },
-    rarityR: {
-      main: "#45b6fe",
-    },
-    info: {
-      main: "#6979d9",
-    },
-  },
-  typography: {
-    fontFamily: `'Roboto', 'Arial', sans-serif`,
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          margin: 0,
-          fontFamily: "'Roboto', sans-serif",
-          backgroundColor: "#f5f5f5",
-        },
-      },
-    },
-  },
-});
+import "./index.css";
 
 const CharacterOverlayWrapper: React.FC = () => {
   const { name } = useParams<{ name: string }>();
@@ -82,19 +33,7 @@ const CharacterOverlayWrapper: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalStyles
-        styles={{
-          body: {
-            backgroundColor: "background.default",
-            margin: 0,
-            padding: 0,
-            height: "100%",
-            width: "100%",
-          },
-        }}
-      />
+    <div className="min-h-screen bg-neutral-200 text-neutral-700">
       <Router>
         <Layout>
           <Routes>
@@ -105,7 +44,7 @@ const App: React.FC = () => {
           </Routes>
         </Layout>
       </Router>
-    </ThemeProvider>
+    </div>
   );
 };
 
