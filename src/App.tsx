fb895af0295b4eb7c44d96e99da5325a6d435de1
xuda <xudacn0@gmail.com>
2025-04-21 21:18:41 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, useParams, useNavigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+  useNavigate,
+  useLocation,
+} from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './layouts/Layout';
 import Home from './pages/Home';
 import CharacterGrid from './pages/CharacterGrid';
@@ -12,6 +20,22 @@ import { formatWeaponUrl } from './utils/wpn-utils';
 import './index.css';
 
 const baseUrl = import.meta.env.BASE_URL;
+
+const RedirectionHandler: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const savedPath = sessionStorage.getItem('redirectPath');
+
+    if (savedPath && location.pathname === '/') {
+      sessionStorage.removeItem('redirectPath');
+      navigate(savedPath);
+    }
+  }, [navigate, location]);
+
+  return <>{children}</>;
+};
 
 const CharacterOverlayWrapper: React.FC = () => {
   const { url } = useParams<{ url: string }>();
@@ -45,16 +69,19 @@ const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-neutral-200 text-neutral-700">
       <Router basename={baseUrl}>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dolls" element={<CharacterGrid />} />
-            <Route path="/dolls/:url" element={<CharacterOverlayWrapper />} />
-            <Route path="/weapons" element={<WeaponGrid />} />
-            <Route path="/weapons/:url" element={<WeaponOverlayWrapper />} />
-            <Route path="/enemies" element={<EnemyGrid />} />
-          </Routes>
-        </Layout>
+        <RedirectionHandler>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dolls" element={<CharacterGrid />} />
+              <Route path="/dolls/:url" element={<CharacterOverlayWrapper />} />
+              <Route path="/weapons" element={<WeaponGrid />} />
+              <Route path="/weapons/:url" element={<WeaponOverlayWrapper />} />
+              <Route path="/enemies" element={<EnemyGrid />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </Layout>
+        </RedirectionHandler>
       </Router>
     </div>
   );
